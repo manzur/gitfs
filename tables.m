a5a300d39da83fc5d8e41ab946790e153243301c
Manzur <manzur@UCB.(none)>
2009-06-24 17:42:47 +0400

    -Added modified version of tables module;

---
+++ tables.m
@@ -0,0 +1,26 @@
+Tables: module {
+	PATH: con "tables.dis";
+	Table: adt[T] {
+		items:	array of list of (int, T);
+		nilval:	T;
+	
+		new: fn(nslots: int, nilval: T): ref Table[T];
+		add:	fn(t: self ref Table, id: int, x: T): int;
+		del:	fn(t: self ref Table, id: int): int;
+		find:	fn(t: self ref Table, id: int): T;
+		all:    fn(t: self ref Table): list of T;
+	};
+	
+	Strhash: adt[T] {
+		items:	array of list of (string, T);
+		nilval:	T;
+	
+		new: fn(nslots: int, nilval: T): ref Strhash[T];
+		add:	fn(t: self ref Strhash, id: string, x: T);
+		del:	fn(t: self ref Strhash, id: string);
+		find:	fn(t: self ref Strhash, id: string): T;
+		all:    fn(t: self ref Strhash): list of T;
+	};
+
+	hash: fn(s: string, n: int): int;
+};
